db5c3819b13ac0dfc72bddc1d3de4fac64815d42
Ioannis Kalaitzoglou <johnkalai44@gmail.com>
2022-03-29 12:53:19 +0200

    Cleaned CreateRandomObject Script + objects snap to OVRhands

--- Assets/Scripts/CreateRandomObject.cs
+++ Assets/Scripts/CreateRandomObject.cs
@@ -7,8 +7,6 @@ public class CreateRandomObject : MonoBehaviour
     public GameObject[] shapes;
     public Color[] colors;
     public List<GameObject> createdObjects;
-    private int indexTable = 1;
-    private bool firstCollisionEnter = false;
     private bool hasEntered;
     void Start()
     {
@@ -37,23 +35,23 @@ public class CreateRandomObject : MonoBehaviour
         GameObject obj = Instantiate(shapes[chooseItem], blockCentre, Quaternion.identity) as GameObject;
         obj.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
 
+        /*
         Debug.Log("Color" + obj.GetComponent<Renderer>().material.color);
         Debug.Log("OBJECT CREATED");
-
+        */
         if (this.gameObject.name == "Table1")
         {
-            Debug.Log("Tag set to table1");
+           // Debug.Log("Tag set to table1");
             obj.gameObject.tag = "Table1";
         }
         else
         {
-            Debug.Log("Tag set to table2");
+           // Debug.Log("Tag set to table2");
             obj.gameObject.tag = "Table2";
         }
-        hasEntered = false;
+        hasEntered = false;       
        
-        return obj;    
-     
+        return obj;         
     }
 
     //Create new object when touching grabbed object to the opposite table. 
@@ -61,9 +59,10 @@ public class CreateRandomObject : MonoBehaviour
 	{
         if (!hasEntered && (collision.gameObject.tag != this.gameObject.name) && (collision.gameObject.tag == "Table1" || collision.gameObject.tag == "Table2"))
         {
-            Debug.Log(collision.gameObject.tag + " COLLIDES WITH " + this.gameObject.name);
+           // Debug.Log(collision.gameObject.tag + " COLLIDES WITH " + this.gameObject.name);
             hasEntered = true;
             CreateObject();
+            // FOR NOW THE OBJECT IS DESTROYED. THIS WILL CHANGE
             Destroy(collision.gameObject);
         }           
 	}
