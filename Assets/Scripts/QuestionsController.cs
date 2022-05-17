930bed947431ba3baea0d6de9975c3b64578e879
Ioannis Kalaitzoglou <johnkalai44@gmail.com>
2022-05-17 15:32:57 +0200

    2

--- Assets/Scripts/QuestionsController.cs
+++ Assets/Scripts/QuestionsController.cs
@@ -133,6 +133,7 @@ public class QuestionsController : MonoBehaviour
         yield return new WaitForSeconds(seconds);        
         CreateQuestionPrompts();
         this.transform.position = new Vector3(camera.transform.position.x + (camera.transform.forward.x * distanceFromCamera), player.transform.position.y + (player.transform.forward.y * distanceFromCamera) + 0.4f, 0f);        
+       
         this.transform.Rotate(0, 180, 0);
         seconds = 0;
     }
